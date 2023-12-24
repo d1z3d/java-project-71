09c136ac75df81b32aef58b426bd3853d9a72742
Andrey Borychev <borychev.au@gmail.com>
2023-12-25 01:20:22 +0300

    fix Differ

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -16,6 +16,18 @@ public class Differ {
         Map<String, Object> map2 = Util.getData(dataFromSecondFile);
 
         List<DiffString> diffStrings = new ArrayList<>();
+
+        fillResult(map1, map2, diffStrings);
+
+        for (String key : map2.keySet()) {
+            diffStrings.add(new DiffString(2, "+", key, map2.get(key)));
+        }
+        Collections.sort(diffStrings);
+
+        return DiffString.listToString(diffStrings);
+    }
+
+    private static void fillResult(Map<String, Object> map1, Map<String, Object> map2, List<DiffString> result) {
         String key1;
         String key2;
         Object value1;
@@ -27,27 +39,17 @@ public class Differ {
             key2 = map2.containsKey(key1) ? key1 : null;
             value2 = map2.getOrDefault(key2, null);
             if (key2 == null) {
-                diffStrings.add(new DiffString(1, "-", key1, value1));
+                result.add(new DiffString(1, "-", key1, value1));
             } else {
                 if (value1.equals(value2)) {
-                    diffStrings.add(new DiffString(1, " ", key1, value1));
+                    result.add(new DiffString(1, " ", key1, value1));
                     map2.remove(key2);
                 } else {
-                    diffStrings.add(new DiffString(1, "-", key1, value1));
-                    diffStrings.add(new DiffString(2, "+", key2, value2));
+                    result.add(new DiffString(1, "-", key1, value1));
+                    result.add(new DiffString(2, "+", key2, value2));
                     map2.remove(key2);
                 }
             }
         }
-
-        for (String key : map2.keySet()) {
-            key2 = key;
-            value2 = map2.get(key2);
-
-            diffStrings.add(new DiffString(2, "+", key2, value2));
-        }
-        Collections.sort(diffStrings);
-
-        return DiffString.listToString(diffStrings);
     }
 }
