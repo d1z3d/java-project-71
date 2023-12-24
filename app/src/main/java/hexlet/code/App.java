495c9a7a689f3c16f6ca1ec23a7103394f69a276
Andrey Borychev <borychev.au@gmail.com>
2023-12-24 14:15:38 +0300

    add set format

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,11 +4,27 @@ package hexlet.code;
 
 import picocli.CommandLine;
 
+import java.io.File;
 import java.util.concurrent.Callable;
 
-@CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0", description = "Compares two configuration files and shows a difference.")
+@CommandLine.Command(name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
 
+    @CommandLine.Parameters(index = "0",
+            description = "path to first file")
+    private File filepath1;
+    @CommandLine.Parameters(index = "1",
+            description = "path to second file")
+    private File filepath2;
+
+    @CommandLine.Option(names = {"-f", "--format"},
+            defaultValue = "stylish",
+            required = false,
+            description = "out format [default: stylish]")
+    private String format = "stylish";
     @Override
     public Integer call() throws Exception {
         System.out.println("test");
