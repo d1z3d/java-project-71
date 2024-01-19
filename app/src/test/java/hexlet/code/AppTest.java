9203ad14fd2dfce39043ba3b3a50814eea7517fc
Andrey Borychev <borychev.au@gmail.com>
2024-01-19 23:25:55 +0300

    fix checkstyleTest

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -7,8 +7,6 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 
 public class AppTest {
 
