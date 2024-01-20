999264e1bb4acb73637ad0ddaae0b902dc7a4db7
Andrey Borychev <borychev.au@gmail.com>
2024-01-20 13:20:44 +0300

    fix test linter

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -2,11 +2,6 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DifferTest {
--- app/src/test/java/hexlet/code/TestUtils.java
+++ app/src/test/java/hexlet/code/TestUtils.java
@@ -1,7 +1,6 @@
 package hexlet.code;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.nio.file.Paths;
