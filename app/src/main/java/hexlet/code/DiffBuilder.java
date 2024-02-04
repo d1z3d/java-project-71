63525be691679b06a260ddd654ca05b7615d12d1
Andrey Borychev <borychev.au@gmail.com>
2024-02-04 19:56:54 +0300

    fix naming

--- app/src/main/java/hexlet/code/DiffBuilder.java
+++ app/src/main/java/hexlet/code/DiffBuilder.java
@@ -9,23 +9,23 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class DiffBuilder {
-    public static List<Map<String, Object>> compareData(Map<String, Object> firstMap, Map<String, Object> secondMap) {
-        Set<String> keys = new TreeSet<>(firstMap.keySet());
-        keys.addAll(secondMap.keySet());
+    public static List<Map<String, Object>> compareData(Map<String, Object> data1, Map<String, Object> data2) {
+        Set<String> keys = new TreeSet<>(data1.keySet());
+        keys.addAll(data2.keySet());
 
         List<Map<String, Object>> result = new ArrayList<>();
         for (String key : keys) {
             Map<String, Object> node;
-            Object value1 = firstMap.get(key);
-            Object value2 = secondMap.get(key);
-            if (!firstMap.containsKey(key)) {
-                node = createNode(key, "added", secondMap.get(key), null);
-            } else if (!secondMap.containsKey(key)) {
-                node = createNode(key, "removed", firstMap.get(key), null);
+            Object value1 = data1.get(key);
+            Object value2 = data2.get(key);
+            if (!data1.containsKey(key)) {
+                node = createNode(key, "added", data2.get(key), null);
+            } else if (!data2.containsKey(key)) {
+                node = createNode(key, "removed", data1.get(key), null);
             } else if (Objects.equals(value1, value2)) {
-                node = createNode(key, "equals", firstMap.get(key), secondMap.get(key));
+                node = createNode(key, "equals", data1.get(key), data2.get(key));
             } else {
-                node = createNode(key, "updated", firstMap.get(key), secondMap.get(key));
+                node = createNode(key, "updated", data1.get(key), data2.get(key));
             }
             result.add(node);
         }
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -7,9 +7,9 @@ public class Differ {
     public static String generate(String path1, String path2, String format) throws Exception {
         String dataFromFirstFile = Utils.getDataFromFile(path1);
         String dataFromSecondFile = Utils.getDataFromFile(path2);
-        Map<String, Object> mapData1 = Parser.parse(dataFromFirstFile, Utils.getExtensionByString(path1));
-        Map<String, Object> mapData2 = Parser.parse(dataFromSecondFile, Utils.getExtensionByString(path2));
-        List<Map<String, Object>> comparedData = DiffBuilder.compareData(mapData1, mapData2);
+        Map<String, Object> data1 = Parser.parse(dataFromFirstFile, Utils.getExtensionByString(path1));
+        Map<String, Object> data2 = Parser.parse(dataFromSecondFile, Utils.getExtensionByString(path2));
+        List<Map<String, Object>> comparedData = DiffBuilder.compareData(data1, data2);
         return Formatter.format(comparedData, format);
     }
 
